32895557be9a15806b71555890006751c51980a4
ThiagoCmrg <thiagosantoscamargo@hotmail.com>
2025-09-20 13:15:38 -0300

    Estrutura atualizada, pronto pra desenvolvimento

--- element_monster.go
+++ element_monster.go
@@ -1,3 +1,8 @@
 package main
 
-func (m *Monster) Run(ctx context.Context, out chan<- GameEvent, alerts <-chan PlayerAlert, pstate <-chan PlayerState)
+import "context"
+
+func (m *Monster) Run(ctx context.Context, out chan<- GameEvent, alerts <-chan PlayerAlert, pstate <-chan PlayerState) {
+	// TODO: Implementar lógica do Monster
+	// Por enquanto, função vazia para compilar
+}
